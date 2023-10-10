d99e5bf1e5258d67fc5ff5b18bf5e28037bf3636
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-10 13:09:37 -0400

    Typescript ver

--- main.ts
+++ main.ts
@@ -1,8 +1,52 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by Ernest
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+let rockPaperScissors: number = -1
+let scoreValue: number = 0
+
+// on shake
+input.onGesture(Gesture.Shake, function() {
+  rockPaperScissors = randint(0 , 2)
+
+if (rockPaperScissors == 0) {
+basic.showIcon(IconNames.Scissors)
+}
+
+if (rockPaperScissors == 1) { 
+  basic.showLeds(`
+  # # # # #
+  # . . . #
+  # . . . #
+  # . . . #
+  # # # # #
+  `)
+}
+
+if (rockPaperScissors == 2) {
+  basic.showLeds(`
+  . . . . .
+  . # # # .
+  . # # # .
+  . # # # .
+  . . . . .
+  `)
+}
+
+basic.pause(1000)
+basic.clearScreen()
+})
+// when A button is pressed
+input.onButtonPressed(Button.A, function() {
+  basic.showIcon(IconNames.Yes)
+  scoreValue = scoreValue + 1
+})
+//when B button is pressed
+input.onButtonPressed(Button.B, function () {
+  basic.showString('Score is:' + (scoreValue).toString())
+})
