14017c433d37d8d8effeb763f9880dc787d06ff9
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-18 02:08:18 +0000

    fix soon

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,40 +7,39 @@ This module is a Micro:bit MicroPython program
 from random import *
 from microbit import *
 
+rock_paper_scissors = -1
+score_value = 0
 
 display.clear
 display.show(Image.HAPPY)
 
-rock_paper_scissors = -1
-score_value = 0
-
-
-while True:
-    if accelerometer.was_gesture("shake"):
-        rock_paper_scissors = randint(0, 2)
-
+gesture = accelerometer.current_gesture()
+if gesture == "shake":
+    rock_paper_scissors = randint(0, 2)
+    # if number is 0 show paper
     if rock_paper_scissors == 0:
-        rock = Image("99009:" "99090:" "99900:" "99090:" "99009:")
-        display.show(rock)
-        sleep(1000)
+        display.show(Image.SQUARE)
+        sleep(5000)
         display.show(Image.HAPPY)
 
+    # if number is 1 show rock
     if rock_paper_scissors == 1:
-        scissors = Image("00000:" "09990:" "09990:" "09990:" "00000:")
-        display.show(scissors)
-        sleep(1000)
+        display.show(Image.SQUARE_SMALL)
+        sleep(5000)
         display.show(Image.HAPPY)
 
+    # If number is 2 show scissor
     if rock_paper_scissors == 2:
-        paper = Image("99999:" "90009:" "90009:" "90009:" "99999:")
-        display.show(paper)
-        sleep(1000)
+        scissors = Image("99009:" "99090:" "99900:" "99090:" "99009:")
+        display.show(scissors)
+        sleep(5000)
         display.show(Image.HAPPY)
 
     if button_a.is_pressed():
         score_value = score_value + 1
         display.show(Image.YES)
         sleep(1000)
+        display.clear()
         display.show(Image.HAPPY)
 
     if button_b.is_pressed():
