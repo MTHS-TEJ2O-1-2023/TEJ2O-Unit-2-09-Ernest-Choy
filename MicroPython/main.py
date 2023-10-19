fda23bc6e25d5a9a5ef20ae81b01cd660685088b
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-19 01:32:18 +0000

    FIXED PYVER

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -15,27 +15,28 @@ display.show(Image.HAPPY)
 
 while True:
     gesture = accelerometer.current_gesture()
-    if gesture == "shake":
+    if accelerometer.was_gesture("shake"):
         rock_paper_scissors = randint(0, 2)
+
         # if number is 0 show scissors
         if rock_paper_scissors == 0:
             scissors = Image("99009:" "99090:" "99900:" "99090:" "99009:")
             display.show(scissors)
-            sleep(5000)
+            sleep(2000)
             display.show(Image.HAPPY)
 
         # if number is 1 show rock
         if rock_paper_scissors == 1:
             rock = Image("00000:" "09990:" "09990:" "09990:" "00000:")
             display.show(rock)
-            sleep(5000)
+            sleep(2000)
             display.show(Image.HAPPY)
 
         # If number is 2 show paper
         if rock_paper_scissors == 2:
             paper = Image("99999:" "90009:" "90009:" "90009:" "99999:")
             display.show(paper)
-            sleep(5000)
+            sleep(2000)
             display.show(Image.HAPPY)
 
     if button_a.is_pressed():
